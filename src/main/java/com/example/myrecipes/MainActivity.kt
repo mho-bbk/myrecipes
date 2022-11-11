9d05237bc8e108c339e3c4d1de5a125756796847
Kitty Mo Yin Ho <kmoyinho@gmail.com>
2022-11-11 11:29:18 +0000

    Set up viewbinding

--- src/main/java/com/example/myrecipes/MainActivity.kt
+++ src/main/java/com/example/myrecipes/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myrecipes.converter.ConverterActivity
+import com.example.myrecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var listIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
     }
 
     private fun launchConverter() {
