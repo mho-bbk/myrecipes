90be487fce4957d87404adef372b250f20a9b558
Kitty Mo Yin Ho <kmoyinho@gmail.com>
2022-11-11 11:02:36 +0000

    Added intent: Home -> Converter

--- src/main/java/com/example/myrecipes/MainActivity.kt
+++ src/main/java/com/example/myrecipes/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myrecipes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myrecipes.converter.ConverterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun launchConverter() {
+        listIntent = Intent(this, ConverterActivity::class.java)
+        startActivity(listIntent)
     }
 }
