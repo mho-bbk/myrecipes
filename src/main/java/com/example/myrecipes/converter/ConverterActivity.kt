cbf2af1b23978d40aa3c41bb9fd8ec187575cdf5
Kitty Mo Yin Ho <kmoyinho@gmail.com>
2022-11-11 10:25:05 +0000

    First activities, no logic

---
+++ src/main/java/com/example/myrecipes/converter/ConverterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myrecipes.converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myrecipes.R
+
+class ConverterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_converter)
+    }
+}
