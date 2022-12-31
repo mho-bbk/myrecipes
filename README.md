6e7f3bca3012b2b60198dbdf87e2896869d8658f
kmoyinho <kmoyinho@gmail.com>
2022-12-31 17:02:51 +0000

    Refactored ConverterViewModel.kt, added notes

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Fixes
-[ ] When phone rotates, ensure full screen is visible
-[ ] When phone rotates, ensure conversion is retained
+[ ] In horizontal mode, there should be a space of at least X dp between the conversion text 
+    and the end of the screen
+[ ] Increase the font size for the conversion text
+[x] When phone rotates, ensure full screen is visible - implemented ScrollView
+[x] When phone rotates, ensure conversion is retained - implemented ViewModel
 
 # New Features
 + Improve UI (make prettier) while adhering to accessibility guidelines
-+ Add tablespoon conversion
-+ Add farenheit conversion
++ Converter - Add tablespoon conversion (design idea: make spinner for 'cups' selection)
++ Converter - Add farenheit conversion
++ (EPIC) Recipes Collection - Enables user to note down and keep recipes in a local cache
++ (EPIC) Save Conversions - Enables user to save conversions to their recipes collection. 
+   Must have provision to save multiple conversions to one recipe.
++ (EPIC) Export feature, to ensure recipes collection can be backed up if local storage fails. 
--- src/main/java/com/example/myrecipes/converter/ConverterViewModel.kt
+++ src/main/java/com/example/myrecipes/converter/ConverterViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 class ConverterViewModel : ViewModel() {
 
+    // TODO - ingredientQuantity and convertedIngredientQuantity perhaps not necessary
+    //  as separate LiveData fields
     private var _ingredientQuantity = MutableLiveData(0.00)
-    val ingredientQuantity: LiveData<Double>
+    private val ingredientQuantity: LiveData<Double>
         get() = _ingredientQuantity
 
     private var _convertedIngredientQuantity = MutableLiveData(0.00)
-    val convertedIngredientQuantity: LiveData<Double>
+    private val convertedIngredientQuantity: LiveData<Double>
         get() = _convertedIngredientQuantity
 
     private var _convertedString = MutableLiveData("")
@@ -18,17 +20,17 @@ class ConverterViewModel : ViewModel() {
         get() = _convertedString
 
     /**
-     * Converts cups to grams and displays it on the screen
+     * Converts cups to grams
      */
     fun convert(cupString: String, ingredient: String) {
         _ingredientQuantity.value = cupString.toDoubleOrNull()
 
         // Value of conversion depends on ingredient selected
         val ingredientDensity = when(ingredient) {
-            "Flour" -> 125.16
-            "Sugar" -> 200.86
-            "Butter" -> 226.89
-            else -> 244.87 //is Milk
+            "Flour" -> FLOUR
+            "Sugar" -> SUGAR
+            "Butter" -> BUTTER
+            else -> MILK
         }
 
         _convertedIngredientQuantity.value = ingredientDensity * _ingredientQuantity.value!!
@@ -47,6 +49,10 @@ class ConverterViewModel : ViewModel() {
     companion object {
         const val x_cups_to_grams = "%.2f cups to %.2f grams"
         const val invalid_input = "Invalid value. Try again."
+        const val FLOUR = 125.16
+        const val SUGAR = 200.86
+        const val BUTTER = 226.89
+        const val MILK = 244.87
     }
 }
 
