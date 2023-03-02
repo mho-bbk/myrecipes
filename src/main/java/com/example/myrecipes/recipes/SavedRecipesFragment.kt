41058f57da7dd5a13c8d3c0cc4fda60efac7f655
kmoyinho <kmoyinho@gmail.com>
2023-03-02 18:55:18 +0000

    Refactored to use data binding again

--- src/main/java/com/example/myrecipes/recipes/SavedRecipesFragment.kt
+++ src/main/java/com/example/myrecipes/recipes/SavedRecipesFragment.kt
@@ -1,41 +1,50 @@
 package com.example.myrecipes.recipes
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myrecipes.R
 import com.example.myrecipes.adapters.RecipeAdapter
+import com.example.myrecipes.databinding.FragmentRecipesSavedBinding
 
 class SavedRecipesFragment : Fragment(R.layout.fragment_recipes_saved) {
 
     private val viewModel: RecipesViewModel by activityViewModels()
 
+    private lateinit var binding: FragmentRecipesSavedBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment with *data* binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes_saved, container, false)
+
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        setupRecyclerView()
-
-        val recyclerView = requireActivity().findViewById<RecyclerView>(R.id.saved_recipes)
         val recipeAdapter = RecipeAdapter()
-        recyclerView.apply {
-            adapter = recipeAdapter
-            layoutManager = LinearLayoutManager(activity)
-        }
+        setupRecyclerView(recipeAdapter)
 
         viewModel.allRecipes.observe(viewLifecycleOwner) { recipes ->
             recipes?.let { recipeAdapter.submitList(it) }
         }
     }
-//
-//    private fun setupRecyclerView() {
-//        recipeAdapter = RecipeAdapter()
-//        binding.savedRecipes.apply {
-//            adapter = recipeAdapter
-//            layoutManager = LinearLayoutManager(activity)
-//        }
-//    }
+
+    private fun setupRecyclerView(recipeAdapter: RecipeAdapter) {
+        binding.savedRecipes.apply {
+            adapter = recipeAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 
 }
